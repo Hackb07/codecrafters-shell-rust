3d82bdc1a70ba7d2a5679876d7251e8c6d33556c
Hackb07 <balat4880@gmail.com>
2026-05-23 20:26:18 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
+use std::fs;
 use std::io::{self, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
@@ -64,45 +65,63 @@ fn main() {
             }
 
             // External commands
-            _ => {
-                match find_executable(command) {
-                    Some(path) => {
-                        // Get parent directory
-                        let parent = path.parent().unwrap();
+            _ => match find_executable(command) {
+                Some(path) => {
+                    let result = Command::new(&path).args(args).spawn();
 
-                        let result = Command::new(command).args(args).current_dir(parent).spawn();
+                    match result {
+                        Ok(mut child) => {
+                            child.wait().unwrap();
+                        }
 
-                        match result {
-                            Ok(mut child) => {
-                                child.wait().unwrap();
-                            }
-
-                            Err(_) => {
-                                println!("{}: command not found", command);
-                            }
+                        Err(_) => {
+                            println!("{}: command not found", command);
                         }
                     }
-
-                    None => {
-                        println!("{}: command not found", command);
-                    }
                 }
-            }
+
+                None => {
+                    println!("{}: command not found", command);
+                }
+            },
         }
     }
 }
 
-// Search executable in PATH
+// Find executable in PATH
 fn find_executable(command: &str) -> Option<PathBuf> {
     let path_env = env::var("PATH").unwrap_or_default();
 
     for dir in env::split_paths(&path_env) {
         let full_path = dir.join(command);
 
-        if full_path.is_file() {
+        if full_path.is_file() && is_executable(&full_path) {
             return Some(full_path);
         }
     }
 
     None
+}
+
+// Check execute permissions
+fn is_executable(path: &Path) -> bool {
+    #[cfg(unix)]
+    {
+        use std::os::unix::fs::PermissionsExt;
+
+        if let Ok(metadata) = fs::metadata(path) {
+            let permissions = metadata.permissions();
+            let mode = permissions.mode();
+
+            // Any execute bit set
+            return mode & 0o111 != 0;
+        }
+    }
+
+    #[cfg(windows)]
+    {
+        return path.is_file();
+    }
+
+    false
 }
